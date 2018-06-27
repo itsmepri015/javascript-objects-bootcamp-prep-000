87ee6a935d6d057a5721dde2d189798425ee35e7
naizal Edy <pritiadhikari@gmail.com>
2018-06-27 20:01:46 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist = { 
+  avril: 'my happy ending',
+  bryan: 'inside out',
+  ed: 'prefect'
+};
+
+function updatePlaylist(playlist,aristName,songTitle)
+{
+  
+}
+
+function removeFromPlaylist(playlist,aristName)
+{
+  
+  delete playlist.aristName;
+  
+}
